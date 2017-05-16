d9f064c0013e4cbe0bf06785cfea83cccb092bb6
Sean McDermott <mcderm.sean@gmail.com>
2017-05-16 10:43:34 -0700

    first two problemsets complete

--- src/00_wine.js
+++ src/00_wine.js
@@ -1,9 +1,20 @@
-const oneToMany = (one, many, newKey, foreignKey) => {
-  
+const oneToMany = (many, one, newKey, foreignKey) => {
+  one.forEach(oneEl => {
+    oneEl[newKey] = many.filter(manyEl => {
+      return manyEl[foreignKey] === oneEl.id;
+    })
+  })
+  return one;
 }
 
-const belongsTo = (one, many, newKey, foreignKey) => {
-
+const belongsTo = (many, one, newKey, foreignKey) => {
+  many.forEach(manyEl => {
+    manyEl[newKey] = one.filter(oneEl => {
+      return oneEl.id === manyEl[foreignKey];
+    })[0]
+    delete manyEl[foreignKey];
+  })
+  return many;
 }
 
 module.exports = { oneToMany, belongsTo }
--- src/01_game.js
+++ src/01_game.js
@@ -3,7 +3,19 @@ const join = (
   resourceB, resourceBForeignKey,
   joinTable, newKey
 ) => {
-  
+  resourceA.forEach(resA => {
+
+    const joinRows = joinTable.filter(row => {
+      return resA.id === row[resourceAForeignKey];
+    })
+
+    resA[newKey] = resourceB.filter(resB => {
+      return joinRows.some(row => {
+        return row[resourceBForeignKey] === resB.id
+      })
+    })
+  })
+  return resourceA;
 }
 
 module.exports = { join }
